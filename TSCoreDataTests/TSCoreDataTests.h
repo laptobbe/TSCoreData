b69d26fdd8e477c0c57fe0517ed45882d5c94ceb
Tobias Sundstrand <tobias@freeb.ee>
2013-05-23 19:25:33 +0200

    Initial Commit

---
+++ TSCoreDataTests/TSCoreDataTests.h
@@ -0,0 +1,13 @@
+//
+//  TSCoreDataTests.h
+//  TSCoreDataTests
+//
+//  Created by Tobias Sundstrand on 2013-05-23.
+//  Copyright (c) 2013 Computertalk Sweden. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TSCoreDataTests : SenTestCase
+
+@end
---
+++ TSCoreDataTests/TSCoreDataTests.m
@@ -0,0 +1,32 @@
+//
+//  TSCoreDataTests.m
+//  TSCoreDataTests
+//
+//  Created by Tobias Sundstrand on 2013-05-23.
+//  Copyright (c) 2013 Computertalk Sweden. All rights reserved.
+//
+
+#import "TSCoreDataTests.h"
+
+@implementation TSCoreDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TSCoreDataTests");
+}
+
+@end
