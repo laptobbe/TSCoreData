f2366ba5395427176ecb6fe5a1d089ffda306bdb
Tobias Sundstrand <tobias@freeb.ee>
2014-04-11 18:15:50 +0200

    Added same tests as for SQL store.

--- TSCoreDataTests/TSBinaryCoreDataStackTests.m
+++ TSCoreDataTests/TSBinaryCoreDataStackTests.m
@@ -7,8 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <TSAsyncTesting/TSAsyncTesting.h>
 #import "TSCoreDataStackTestCase.h"
-#import "TSAbstractCoreDataStack.h"
 #import "TSBinaryCoreDataStack.h"
 #import "Chair.h"
 #import "NSManagedObject+TSCoreData.h"
@@ -66,4 +66,56 @@
     XCTAssertEqual(tables.count, 0U);
 }
 
+- (void)testCreatingEntity {
+    Chair *chair = [[Chair alloc] initWithManagedObjectContext:self.coreData.threadSpecificContext];
+    XCTAssertNotNil(chair);
+    XCTAssertNotNil(chair.objectID);
+}
+
+- (void)testSettingBasicProperty {
+    Chair *chair = [[Chair alloc] initWithManagedObjectContext:self.coreData.threadSpecificContext];
+    chair.material = @"Wood";
+    XCTAssertEqualObjects(@"Wood", chair.material);
+}
+
+- (void)testFetchingWithoutSavingContext {
+    [[Chair alloc] initWithManagedObjectContext:self.coreData.threadSpecificContext];
+    NSArray *chairs = [self fetchChairs];
+    XCTAssertEqual(chairs.count, 1U);
+}
+
+- (void)testFetchingInBackgroundWithoutSavingContext {
+    [[Chair alloc] initWithManagedObjectContext:self.coreData.mainManagedObjectContext];
+    [TSAsyncTesting testOnBackgroundQueue:^{
+        NSArray *chairs = [self fetchChairs];
+        XCTAssertEqual(chairs.count, 0U);
+    }];
+}
+
+- (void)testFetchingOnBackgroundThreadAfterSaving {
+    NSManagedObjectContext *context = self.coreData.mainManagedObjectContext;
+    [[Chair alloc] initWithManagedObjectContext:context];
+    NSError *error = nil;
+    [context save:&error];
+    XCTAssertNil(error);
+
+    [TSAsyncTesting testOnBackgroundQueue:^{
+        NSArray *chairs = [self fetchChairs];
+        XCTAssertEqual(chairs.count, 1U);
+    }];
+}
+
+- (void)testSavingOnBackgroundThreadAndFetchingOnMain {
+
+    [TSAsyncTesting testOnBackgroundQueue:^{
+        NSManagedObjectContext *context = self.coreData.threadSpecificContext;
+        [[Chair alloc] initWithManagedObjectContext:context];
+        NSError *error = nil;
+        [context save:&error];
+        XCTAssertNil(error);
+    }];
+
+    NSArray *chairs = [self fetchChairs];
+    XCTAssertEqual(chairs.count, 1U);
+}
 @end
